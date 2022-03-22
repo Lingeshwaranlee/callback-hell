ad5e9d880333e6c04ecdd4a38315dd2c3d150334
lingeshwaran <rishilee03@gmail.com>
2022-03-22 19:55:20 +0530

    removed

---
+++ script.js
@@ -0,0 +1,46 @@
+setTimeout(() => {
+    document.getElementById('display').innerText = "10";
+
+    setTimeout(() => {
+        document.getElementById('display').innerText = "9";
+
+        setTimeout(() => {
+            document.getElementById('display').innerText = "8";
+
+            setTimeout(() => {
+                document.getElementById('display').innerText = "7";
+
+                setTimeout(() => {
+                    document.getElementById('display').innerText = "6";
+
+                    setTimeout(() => {
+                        document.getElementById('display').innerText = "5";
+
+                        setTimeout(() => {
+                            document.getElementById('display').innerText = "4";
+
+                            setTimeout(() => {
+                                document.getElementById('display').innerText = "3";
+
+                                setTimeout(() => {
+                                    document.getElementById('display').innerText = "2";
+
+                                    setTimeout(() => {
+                                        document.getElementById('display').innerText = "1";
+
+                                        setTimeout(() => {
+                                            document.getElementById('display').innerHTML = `<div class="text-center">
+                                                                                           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqspRzxpYOmLs61vgTEcXajLxzPZndGi2QkQ&usqp=CAU" class="img rounded img-thumbnail" alt="img">
+                                                                                               <h3 class="display"> Happy Independence Day...</h3> 
+                                                                                                            </div> `;
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
